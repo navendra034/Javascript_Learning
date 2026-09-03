904eec4dcdeff1599bfb2c3218175163494bc98a
Navendra Yadav <navendra.yadav034@gmail.com>
2026-09-03 17:41:47 +0530

    First commit

---
+++ for-each.js
@@ -0,0 +1,9 @@
+const fruits = ["Apple", "Banana", "Mango"];
+
+// fruits.forEach(function (fruit) {
+//   console.log(fruit);
+// });
+
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
